cdbfbeeb5ae3d9fb552b692b61bf585f9166903b
Stefan Woutersen <165151459+StefanWout@users.noreply.github.com>
2024-08-16 15:51:02 +0000

    added models for cards, users, archetypes and cubes

---
+++ cube_generator/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.contrib.auth.models import User
+ 
+class Archetype(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.CharField(max_length=500)
+    color_identities = models.CharField(max_length=10)  # Assuming it holds concatenated color values (e.g., "WUBRG")
+
+    def __str__(self):
+        return self.name
+
+class Card(models.Model):
+    COLOR_CHOICES = [
+        ('W', 'White'),
+        ('U', 'Blue'),
+        ('B', 'Black'),
+        ('R', 'Red'),
+        ('G', 'Green'),
+        ('C', 'Colorless'),
+        ('WU', 'White-Blue'),
+        ('UB', 'Blue-Black'),
+        ('BR', 'Black-Red'),
+        ('RG', 'Red-Green'),
+        ('GW', 'Green-White'),
+        ('WUBRG', 'All Colors'),  # Add other combinations as needed
+    ]
+
+    name = models.CharField(max_length=255)
+    mana_cost = models.CharField(max_length=50, blank=True, null=True)
+    mana_value = models.DecimalField(max_digits=10, decimal_places=2)
+    type_line = models.CharField(max_length=255)
+    oracle_text = models.CharField(max_length=1000, blank=True, null=True)
+    keywords = models.CharField(max_length=500, blank=True, null=True)
+    power = models.IntegerField(blank=True, null=True)
+    toughness = models.IntegerField(blank=True, null=True)
+    color_identity = models.CharField(max_length=10, choices=COLOR_CHOICES)
+    set_name = models.CharField(max_length=255)
+    rarity = models.CharField(max_length=50)
+    edhrec_rank = models.IntegerField()
+    archetypes = models.ManyToManyField(Archetype)
+    img_url = models.URLField()
+
+    def __str__(self):
+        return self.name
+    
+class Cube(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=1000)
+    cards = models.ManyToManyField(Card)
+    archetypes = models.ManyToManyField(Archetype)
+    color_identities = models.CharField(max_length=10)  
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
