f3b1007be7693ca6485ee59fb93ae96d4c026ab8
Stefan Woutersen <165151459+StefanWout@users.noreply.github.com>
2024-08-20 13:57:30 +0000

    added script for scryfall API to populate the card model, haven't run it yet

--- cube_generator/views.py
+++ cube_generator/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView
 from cube_generator.models import Card
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
--- mtg_commander_cube_generator/urls.py
+++ mtg_commander_cube_generator/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from cube_generator.views import GetCard
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("test/", GetCard.as_view(), name='get_card')
+    path("test/", GetCard.as_view(), name='base')
 ]
